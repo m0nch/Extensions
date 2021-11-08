cfbaf04c646a4b45a5b3b1a85cdb3c7bf39e2dcd
Margarita <Margarita@DATA_SERVER>
2021-11-08 16:58:42 +0400

    Update

--- Extensions/ExtensionClass.cs
+++ Extensions/ExtensionClass.cs
@@ -5,6 +5,11 @@ namespace Extensions
 {
     static class ExtensionClass
     {
+        /// <summary>
+        ///Reverses characters in a given string
+        /// </summary>
+        /// <param name="value">given any string value</param>
+        /// <returns>string value</returns>
         public static string _Reverse(this string value)
         {
             char[] charArray = value.ToCharArray();
@@ -13,16 +18,32 @@ namespace Extensions
             return value;
         }
 
+        /// <summary>
+        /// Checks if a given number is even
+        /// </summary>
+        /// <param name="value">given any Int32 value</param>
+        /// <returns>true or false</returns>
         public static bool _IsEven(this int value)
         {
             return value % 2 == 0;
         }
 
+        /// <summary>
+        /// Checks if a given two number is divided without remainder
+        /// </summary>
+        /// <param name="value1">given any Int32 value</param>
+        /// <param name="value2">given any Int32 value</param>
+        /// <returns>true or false</returns>
         public static bool _IsDividedBy(this int value1, int value2)
         {
             return value1 % value2 == 0;
         }
 
+        /// <summary>
+        /// Converts given list to string
+        /// </summary>
+        /// <param name="list">given any List</param>
+        /// <returns>string value</returns>
         public static string _ConvertToString(this List<string> list)
         {
             string str = default(string);
