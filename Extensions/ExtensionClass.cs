13f6e135890dfb9f4db4ebe5a696e66da1ec5b33
Margarita <Margarita@DATA_SERVER>
2021-11-08 16:44:12 +0400

    Add project files.

---
+++ Extensions/ExtensionClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Extensions
+{
+    static class ExtensionClass
+    {
+        public static string _Reverse(this string value)
+        {
+            char[] charArray = value.ToCharArray();
+            Array.Reverse(charArray);
+            value = new string(charArray);
+            return value;
+        }
+
+        public static bool _IsEven(this int value)
+        {
+            return value % 2 == 0;
+        }
+
+        public static bool _IsDividedBy(this int value1, int value2)
+        {
+            return value1 % value2 == 0;
+        }
+
+        public static string _ConvertToString(this List<string> list)
+        {
+            string str = default(string);
+            for (int i = 0; i < list.Count; i++)
+            {
+                str += list[i] + ", ";
+            }
+            return str;
+        }
+    }
+}
---
+++ Extensions/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello World!";
+            Console.WriteLine(str);
+
+            str = str._Reverse();
+            Console.WriteLine(str);
+
+            int number = 5;
+
+            bool b = number._IsEven();
+            //Console.WriteLine(number._IsEven());
+            if (b)
+            {
+                Console.WriteLine($"{number} is even!");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not even!");
+            }
+            number = 150;
+            b = number._IsDividedBy(5);
+            if (b)
+            {
+                Console.WriteLine($"{number} is divided by 5!");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not divided by 5!");
+            }
+            //Console.WriteLine(number._IsDividedBy(5));
+
+            List<string> list = new List<string>() { "Anna", "Stella", "Christine", "Michelle" };         
+            Console.WriteLine(list._ConvertToString());
+            Console.ReadKey();
+        }
+    }
+}
